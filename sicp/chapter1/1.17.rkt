ba4286e96992ce1013e522dc38c9d99b933a0bb7
Irlirion <grigorjew.rostislaw@gmail.com>
2019-04-08 20:18:07 +0300

    Добавил 1.17

--- sicp/chapter1/1.17.rkt
+++ sicp/chapter1/1.17.rkt
@@ -2,24 +2,17 @@
 
 (#%require rackunit)
 
-(define (* a b)
-  (if (= b 0)
-      0
-      (+ a (* a (dec b)))))
+(define (fast-mult a b)
+  (define (double x)
+    (+ x x))
+  (define (halve x)
+    (/ x 2))
+  (cond ((= b 0) 0)
+        ((even? b) (fast-mult (double a) (halve b)))
+        (else (+ a (fast-mult a (- b 1))))))
 
-(define (square x) (* x x))
-
-(define (double x) (* 2 x))
-
-(define (halve x) (/ x 2))
-
-(define (fast-exp b n)
-  (define (exp-iter b n a)
-    (if (= n 0)
-        a
-        (if (even? n)
-            (exp-iter (square b) (halve n) a)
-            (exp-iter b (- n 1) (* a b)))))
-  (exp-iter b n 1))
-
-(check-equal? (fast-exp 2 10) 1024)
+(check-equal? (fast-mult 2 10) 20)
+(check-equal? (fast-mult 0 10) 0)
+(check-equal? (fast-mult 0 0) 0)
+(check-equal? (fast-mult -100 100) -10000)
+(check-equal? (fast-mult 100 100) 10000)
